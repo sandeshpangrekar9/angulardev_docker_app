084b7ff171b231a4810cd1ac57c9991dbc852920
sandeshpangrekar9 <sandeshpangrekar@gmail.com>
2024-12-29 21:28:12 +0530

    updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ng-docker' title`, () => {
+  it(`should have the 'angulardev_docker_app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-docker');
+    expect(app.title).toEqual('angulardev_docker_app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-docker');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angulardev_docker_app');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-docker';
+  title = 'angulardev_docker_app';
 }
